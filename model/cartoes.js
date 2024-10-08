453b6f2c2fdf8fd56a623ce2271c3abcec315235
Tapioka-788 <guilhermezuchellifelipetto@gmail.com>
2024-10-08 16:43:00 -0300

    mvc

---
+++ model/cartoes.js
@@ -0,0 +1,20 @@
+let cartoes = [
+    {
+        salario: 'salario 10' ,
+        img: ''},
+    {
+        salario: 'salario 11' ,
+        img: ''},
+    {
+        salario: 'salario 12' ,
+        img: ''},
+    {
+        salario: 'salario 13' ,
+        img: ''},
+    {
+        salario: 'salario 14' ,
+        img: ''},
+    {
+        salario: 'salario 15' ,
+        img: ''}
+]
--- view/js/cartao.js
+++ view/js/cartao.js
@@ -1,24 +1,3 @@
-let cartoes = [
-    {
-        salario: 'salario 10' ,
-        img: ''},
-    {
-        salario: 'salario 11' ,
-        img: ''},
-    {
-        salario: 'salario 12' ,
-        img: ''},
-    {
-        salario: 'salario 13' ,
-        img: ''},
-    {
-        salario: 'salario 14' ,
-        img: ''},
-    {
-        salario: 'salario 15' ,
-        img: ''}
-]
-
 export function criarCartoes() {
 
     let secitionCartoes = document.getElementById('cartoes')
