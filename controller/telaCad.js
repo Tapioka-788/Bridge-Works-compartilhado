abffe76a7fe119f2a4515feda924cca707586521
Tapioka-788 <guilhermezuchellifelipetto@gmail.com>
2024-10-11 22:45:45 -0300

    socoroo

--- controller/telaCad.js
+++ controller/telaCad.js
@@ -1,18 +1,21 @@
 import { cadastrarCartao } from "../services/formacao/cartoes_S.js"
 
 let telaCad = document.getElementById('telaCadastro')
+
 export function escondeTelaCad(){
     telaCad.style.left = '-100vw'
 }
+
 export function mostraTelaCad(){
     telaCad.style.left = '0vw'
 }
+
 export function recolheDados(){
     let salario = document.getElementById('salario')
     let vaga = document.getElementById('vaga')
     let descricao = document.getElementById('descricao')
 
-    cadastrarCartao(salario.value + vaga.value + descricao.value)
+    cadastrarCartao(salario.value, vaga.value, descricao.value)
     escondeTelaCad()
 
     nome.value = '';
--- view/js/cartao_V.js
+++ view/js/cartao_V.js
@@ -1,8 +1,9 @@
 import { mostraTelaCad } from "../../controller/telaCad.js";
-import { excluirCartoes, pegarCartoes } from "../../services/formacao/cartoes_S.js";
+import { excluirCartoes } from "../../services/formacao/cartoes_S.js";
 import { pegarCartoes } from "../../services/formacao/cartoes_S.js"
 
 export async function criarCartoes() {
+    
     let secitionCartoes = document.getElementById('cartoes')
     secitionCartoes.innerHTML = ''
 
@@ -17,13 +18,18 @@ export async function criarCartoes() {
         let h1 = document.createElement('h1')
         h1.textContent = cartoes[i].salario
         h1.className = 'ct_font';
+
         let h5 = document.createElement('h5')
         h5.textContent = cartoes[i].vaga
         h5.className = 'ct_font';
+
         let p = document.createElement('p')
-        p.textContent = cartoes[i].descricao//adiciona um caroes[i].descricao e add no back end tbm
+        p.textContent = cartoes[i].descricao
+        // p.className = 'description'
+
         let h2 = document.createElement('h2')
         h2.textContent = "88:88"
+        // h2.className = 'time'
 
         let h3 = document.createElement('h3')
         h3.className = 'rodape_card';
@@ -40,7 +46,9 @@ export async function criarCartoes() {
         cartao.appendChild(h5)
         cartao.appendChild(p)
         cartao.appendChild(h2)
+
         cartao.appendChild(button)
+
         cartao.appendChild(h3)
 
         secitionCartoes.appendChild(cartao)
